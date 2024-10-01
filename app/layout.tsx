8829092b3392e0d73b7df1f0f3038ab411bddd5e
Maksad Khalipaev <maks14flay@gmail.com>
2024-10-01 23:57:36 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import "@/app/ui/global.css"
+import { InterFont } from "./ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${InterFont.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from "next/font/google";
+
+export const InterFont = Inter({ subsets: ['latin'] });
+export const LusitanaFont = Lusitana({ subsets: ['latin'], weight: "400" });
